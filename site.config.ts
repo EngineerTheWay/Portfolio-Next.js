0f5b0889b9a086a996196c1207732134aa74103e
Caleb <terillian33@gmail.com>
2025-06-08 16:42:06 -0700

    Add Email Quicknav

--- site.config.ts
+++ site.config.ts
@@ -21,7 +21,7 @@ export default siteConfig({
   github: 'engineertheway',
   linkedin: 'waycaleb',
   // mastodon: '#', // optional mastodon profile URL, provides link verification
-  // newsletter: '#', // optional newsletter URL
+  newsletter: 'contact@calebway.io', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
